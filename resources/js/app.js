3ed55236961e51e0bd1069b6c06a014a125ad1d6
wise-isaac <isaac@warrantywise.co.uk>
2021-07-29 13:39:55 +0100

    create router.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,7 @@
 require('./bootstrap');
+
+import Vue from "vue";
+import VueRouter from "vue-router"
+import "./router"
+
+Vue.use(VueRouter);
---
+++ resources/js/router.js
@@ -0,0 +1,5 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+// import RouterBase from "./components/RouterBase.vue";
+
+Vue.use(VueRouter);
